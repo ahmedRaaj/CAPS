409ef0835102b4bc6cecdda8fb273c9cd3293769
KaungMyatLin <e0046786@u.nus.edu>
2016-12-07 15:08:36 +0800

    AdminC And Repo

--- src/main/java/org/teameleven/caps/controller/AdminController.java
+++ src/main/java/org/teameleven/caps/controller/AdminController.java
@@ -1,15 +1,36 @@
 package org.teameleven.caps.controller;
 
+import org.jboss.logging.Logger;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
+import org.teameleven.caps.model.User;
 import org.teameleven.caps.repository.AdminRepository;
+import org.teameleven.caps.services.AdminService;
 
+/**
+ * 
+ * @author Kaung
+ */
+
+@Controller
+@RequestMapping("/admin")
 public class AdminController {
-	
-	AdminRepository adminDao;
-	
-	public ModelAndView name() {
-		long c = adminDao.count();
-		ModelAndView v = new ModelAndView("Welcome");
-		return v;
-	}
+
+    //AdminService adminServ = new AdminService();
+    @Autowired
+    AdminRepository AdminRepo;
+    
+    @RequestMapping(value = "/create", method = RequestMethod.GET)
+    public ModelAndView newUserAccount() {
+        ModelAndView mav = new ModelAndView("create-newAcc", "user", new User());
+        
+        
+        return mav;
+    }
 }
