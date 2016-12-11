721f42d5b455ce1d7d382a9263a3a134ed1a2cb3
Ahmed Raaj <ahmedraaj@gmail.com>
2016-12-12 01:43:44 +0800

    lecturer grade facility

--- src/main/java/org/teameleven/caps/controller/LecturerController.java
+++ src/main/java/org/teameleven/caps/controller/LecturerController.java
@@ -21,8 +21,10 @@ import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
+import org.teameleven.caps.business.EnrollStatus;
 import org.teameleven.caps.model.Course;
 import org.teameleven.caps.model.EnroledCourse;
+import org.teameleven.caps.model.EnroledCoursePK;
 import org.teameleven.caps.model.Lecturer;
 import org.teameleven.caps.model.Student;
 import org.teameleven.caps.model.User;
@@ -37,214 +39,218 @@ import org.teameleven.caps.repository.LecturerRepository;
 @Controller
 @RequestMapping("/lecturer")
 public class LecturerController {
-    
+
     @Autowired
     EnroledCourseRepository enrolDao;
     @Autowired
     CourseRepository courseDao;
     @Autowired
     LecturerRepository lecDao;
-    
-    
-    
+
     @RequestMapping("/Mainpage")
-    public ModelAndView LecturerMain(RedirectAttributes redirectAttributes,HttpServletRequest req)
-    {
-        User user = (User)req.getSession().getAttribute("user");
+    public ModelAndView LecturerMain(RedirectAttributes redirectAttributes, HttpServletRequest req) {
+        User user = (User) req.getSession().getAttribute("user");
         ModelAndView v = new ModelAndView("/lecturerMainpage");
-        if(user == null){
+        if (user == null) {
             v = new ModelAndView("/unauthorize");
-        }else{
-             v.addObject("user", user);
+        } else {
+            v.addObject("user", user);
         }
-     
+
         return v;
     }
-    
+
     @RequestMapping("/gradecourse")
-    public ModelAndView gradeCourse(HttpServletRequest req)//@RequestParam("lecturerId")String lecturerId)
-    {   
-//        ModelAndView v = new ModelAndView("/lecturer/gradeACourse");
-//               Lecturer l=new Lecturer();
-//     //  l=lecDao.findOne(Integer.parseInt(lecturerId));
-//       v.addObject("enroledcourses", enrolDao.findAll());
-//        v.addObject("courses",courseDao.findAll());
-     //   v.addObject("lecturer",l);
-         ModelAndView v = new ModelAndView("/lecturer/gradeACourse");
+    public ModelAndView gradeCourse(HttpServletRequest req,RedirectAttributes rd)//@RequestParam("lecturerId")String lecturerId)
+    {
+
+        ModelAndView v = new ModelAndView("/lecturer/gradeACourse");
         String pageId = req.getParameter("pageId");
         int pId = 0;
         if (pageId != null && !pageId.equals("")) {
             pId = Integer.parseInt(pageId);
             v.addObject("pageId", pId);
         }
-        PageRequest prEnrol = new PageRequest(pId,10);
+        PageRequest prEnrol = new PageRequest(pId, 10);
         int sizeEnrol = enrolDao.findAll().size();
-        int countEnrol = sizeEnrol / 10+ (sizeEnrol % 10 == 0 ? 0 : 1);
+        int countEnrol = sizeEnrol / 10 + (sizeEnrol % 10 == 0 ? 0 : 1);
         Page<EnroledCourse> pgEnrol = enrolDao.findAll(prEnrol);
-        
+
         PageRequest pr = new PageRequest(pId, 5);
         int size = courseDao.findAll().size();
         int count = size / 5 + (size % 5 == 0 ? 0 : 1);
         Page<Course> pg = courseDao.findAll(pr);
-        
+
         v.addObject("enroledcourses", pgEnrol.getContent());
-        v.addObject("courses",courseDao.findAll());
-      //  v.addObject("count", count);
+        v.addObject("courses", courseDao.findAll());
+        //  v.addObject("count", count);
         v.addObject("countEnrol", countEnrol);
-        return v;
+//        return v;
+    if(req.getAttribute("courseId") == null || req.getAttribute("courseId").equals("")){
+        rd.addAttribute("courseId", 1);
     }
-    
-    
-     @RequestMapping("/viewperformance")
-    public ModelAndView viewPerformance(HttpServletRequest req)
-    {
- 
+      return new ModelAndView("redirect:filter.course");
+    }
+
+    @RequestMapping("/viewperformance")
+    public ModelAndView viewPerformance(HttpServletRequest req) {
+
 //        ModelAndView v = new ModelAndView("/lecturer/viewAStudentPerformance");
 //        v.addObject("enroledcourses", enrolDao.findAll());
 //         v.addObject("courses",courseDao.findAll());
 //        return v;
-        
-      ModelAndView v = new ModelAndView("/lecturer/viewAStudentPerformance");
+        ModelAndView v = new ModelAndView("/lecturer/viewAStudentPerformance");
         String pageId = req.getParameter("pageId");
         int pId = 0;
         if (pageId != null && !pageId.equals("")) {
             pId = Integer.parseInt(pageId);
             v.addObject("pageId", pId);
         }
-        PageRequest prEnrol = new PageRequest(pId,10);
+        PageRequest prEnrol = new PageRequest(pId, 10);
         int sizeEnrol = enrolDao.findAll().size();
-        int countEnrol = sizeEnrol / 10+ (sizeEnrol % 10 == 0 ? 0 : 1);
+        int countEnrol = sizeEnrol / 10 + (sizeEnrol % 10 == 0 ? 0 : 1);
         Page<EnroledCourse> pgEnrol = enrolDao.findAll(prEnrol);
-        
+
         PageRequest pr = new PageRequest(pId, 5);
         int size = courseDao.findAll().size();
         int count = size / 5 + (size % 5 == 0 ? 0 : 1);
         Page<Course> pg = courseDao.findAll(pr);
-        
+
         v.addObject("enroledcourses", pgEnrol.getContent());
-        v.addObject("courses",courseDao.findAll());
-      //  v.addObject("count", count);
+        v.addObject("courses", courseDao.findAll());
+        //  v.addObject("count", count);
         v.addObject("countEnrol", countEnrol);
-        return v;   
+        return v;
     }
-    
+
     @RequestMapping("/viewenrolment")
-    public ModelAndView viewEnrollment(HttpServletRequest req)
-    { 
+    public ModelAndView viewEnrollment(HttpServletRequest req) {
 //        ModelAndView v = new ModelAndView("/lecturer/viewCourseEnrolment");
 //        v.addObject("enroledcourses", enrolDao.findAll());
 //         v.addObject("courses",courseDao.findAll());
 //        return v;
-        
-         ModelAndView v = new ModelAndView("/lecturer/viewCourseEnrolment");
+
+        ModelAndView v = new ModelAndView("/lecturer/viewCourseEnrolment");
         String pageId = req.getParameter("pageId");
         int pId = 0;
         if (pageId != null && !pageId.equals("")) {
             pId = Integer.parseInt(pageId);
             v.addObject("pageId", pId);
         }
-        PageRequest prEnrol = new PageRequest(pId,10);
+        PageRequest prEnrol = new PageRequest(pId, 10);
         int sizeEnrol = enrolDao.findAll().size();
-        int countEnrol = sizeEnrol / 10+ (sizeEnrol % 10 == 0 ? 0 : 1);
+        int countEnrol = sizeEnrol / 10 + (sizeEnrol % 10 == 0 ? 0 : 1);
         Page<EnroledCourse> pgEnrol = enrolDao.findAll(prEnrol);
-        
+
         PageRequest pr = new PageRequest(pId, 5);
         int size = courseDao.findAll().size();
         int count = size / 5 + (size % 5 == 0 ? 0 : 1);
         Page<Course> pg = courseDao.findAll(pr);
-        
+
         v.addObject("enroledcourses", pgEnrol.getContent());
         v.addObject("courses",courseDao.findAll());
-      //  v.addObject("count", count);
+        //  v.addObject("count", count);
         v.addObject("countEnrol", countEnrol);
-        return v;   
+        return v;
     }
-    
+
     @RequestMapping("/viewcourses")
-    public ModelAndView viewCoursesTaught(HttpServletRequest req)
-    {
- 
+    public ModelAndView viewCoursesTaught(HttpServletRequest req) {
+
 //        ModelAndView v = new ModelAndView("/lecturer/viewCourseTaught");
 //        v.addObject("courses", courseDao.findAll());
 //         
 //        return v;
-                 ModelAndView v = new ModelAndView("/lecturer/viewCourseTaught");
+        ModelAndView v = new ModelAndView("/lecturer/viewCourseTaught");
         String pageId = req.getParameter("pageId");
         int pId = 0;
         if (pageId != null && !pageId.equals("")) {
             pId = Integer.parseInt(pageId);
             v.addObject("pageId", pId);
         }
-        PageRequest prEnrol = new PageRequest(pId,10);
+        PageRequest prEnrol = new PageRequest(pId, 10);
         int sizeEnrol = enrolDao.findAll().size();
-        int countEnrol = sizeEnrol / 10+ (sizeEnrol % 10 == 0 ? 0 : 1);
+        int countEnrol = sizeEnrol / 10 + (sizeEnrol % 10 == 0 ? 0 : 1);
         Page<EnroledCourse> pgEnrol = enrolDao.findAll(prEnrol);
-        
+
         PageRequest pr = new PageRequest(pId, 5);
         int size = courseDao.findAll().size();
         int count = size / 5 + (size % 5 == 0 ? 0 : 1);
         Page<Course> pg = courseDao.findAll(pr);
-        
+
         //v.addObject("enroledcourses", pgEnrol.getContent());
-        v.addObject("courses",pg.getContent());
+        v.addObject("courses", pg.getContent());
         v.addObject("count", count);
         //v.addObject("countEnrol", countEnrol);
-        return v;   
+        return v;
     }
-    
+
     private ModelAndView getDebug(String Message) {
         ModelAndView m = new ModelAndView("debug");
         m.addObject("message", Message);
         return m;
     }
 
-    @RequestMapping(value = "/filter.course" , method = RequestMethod.POST)
-    public ModelAndView filterEnroledCourseForGrade(@RequestParam("courseId") String courseId){
+    @RequestMapping(value = "/filter.course")
+    public ModelAndView filterEnroledCourseForGrade(@RequestParam("courseId") String courseId) {
         ModelAndView v = new ModelAndView("/lecturer/gradeACourse");
         int id = Integer.parseInt(courseId);
-        List<EnroledCourse> collect = enrolDao.findAll().stream().filter(e->e.getCourse().getCourseId()==id).collect(Collectors.toList());
+        List<EnroledCourse> collect = enrolDao.findAll().stream().filter(e -> e.getCourse().getCourseId() == id && e.getStatus().equals(EnrollStatus.APPROVED.name())).collect(Collectors.toList());
         v.addObject("enroledcourses", collect);
-        v.addObject("courses",courseDao.findAll());
+        v.addObject("courses", courseDao.findAll());
+        v.addObject("currentCourse",courseId);
+
         return v;
     }
-    
-        @RequestMapping(value = "/filter.enrolcourse" , method = RequestMethod.POST)
-    public ModelAndView filterEnroledCourseForCourseEnrollment(@RequestParam("courseId") String courseId){
+
+    @RequestMapping(value = "/filter.enrolcourse", method = RequestMethod.POST)
+    public ModelAndView filterEnroledCourseForCourseEnrollment(@RequestParam("courseId") String courseId) {
         ModelAndView v = new ModelAndView("/lecturer/viewCourseEnrolment");
         int id = Integer.parseInt(courseId);
-        List<EnroledCourse> collect = enrolDao.findAll().stream().filter(e->e.getCourse().getCourseId()==id).collect(Collectors.toList());
+        List<EnroledCourse> collect = enrolDao.findAll().stream().filter(e -> e.getCourse().getCourseId() == id).collect(Collectors.toList());
         v.addObject("enroledcourses", collect);
-        v.addObject("courses",courseDao.findAll());
+        v.addObject("courses", courseDao.findAll());
         return v;
     }
-    
-            @RequestMapping(value = "/filter.stucourse" , method = RequestMethod.POST)
-    public ModelAndView filterEnroledCourseForPerfomance(@RequestParam("courseId") String courseId){
+
+    @RequestMapping(value = "/filter.stucourse", method = RequestMethod.POST)
+    public ModelAndView filterEnroledCourseForPerfomance(@RequestParam("courseId") String courseId) {
         ModelAndView v = new ModelAndView("/lecturer/viewAStudentPerformance");
         int id = Integer.parseInt(courseId);
-        List<EnroledCourse> collect = enrolDao.findAll().stream().filter(e->e.getCourse().getCourseId()==id).collect(Collectors.toList());
+        List<EnroledCourse> collect = enrolDao.findAll().stream().filter(e -> e.getCourse().getCourseId() == id).collect(Collectors.toList());
         v.addObject("enroledcourses", collect);
-        v.addObject("courses",courseDao.findAll());
+        v.addObject("courses", courseDao.findAll());
         return v;
     }
-    
 
-    
-    @RequestMapping(value = "/submit.grade" , method = RequestMethod.POST)
-    public ModelAndView filterEnroledCourse(HttpServletRequest req){
+    @RequestMapping(value = "/submit.grade", method = RequestMethod.POST)
+    public ModelAndView filterEnroledCourse(HttpServletRequest req) {
         ArrayList<String> params = new ArrayList<String>(req.getParameterMap().keySet());
-       for(String p : params){
+        for (String p : params) {
             int studentId = Integer.parseInt(p);
-            int gradePoint=Integer.parseInt(p);
+            int gradePoint = Integer.parseInt(p);
             return getDebug(studentId + "" + gradePoint);
-       }
-       return getDebug("fssdf");
+        }
+        return getDebug("fssdf");
     }
-    
-     @RequestMapping(value = "/profile")
-    public ModelAndView showProfile(){
+
+    @RequestMapping(value = "/profile")
+    public ModelAndView showProfile() {
         return new ModelAndView("lecturer/lecturer-profile");
-                
-                
+
+    }
+    @RequestMapping(value = "/gradecourse.submit")
+    public ModelAndView gradeSubmit(@RequestParam("courseId") String courseId, 
+                                    @RequestParam("studentId") String studentId,
+                                    @RequestParam("grade") String grade) {
+        EnroledCoursePK key = new EnroledCoursePK(Integer.parseInt(studentId), Integer.parseInt(courseId));
+        EnroledCourse enrol = enrolDao.findOne(key);
+        if(enrol != null){
+            enrol.setStatus(EnrollStatus.COMPLETED.name());
+            enrol.setGradePoint(grade);
+            enrolDao.save(enrol);
+        }
+        return new ModelAndView("redirect:gradecourse");
+      // return getDebug(enrol.getGradePoint());
     }
 }
--- src/main/webapp/WEB-INF/pages/lecturer/gradeACourse.jsp
+++ src/main/webapp/WEB-INF/pages/lecturer/gradeACourse.jsp
@@ -14,87 +14,103 @@
         <title>Grade a course Page</title>
     </head>
     <body>      
-        
+
         <div class="container">
-        <form action="${pageContext.request.contextPath}/lecturer/filter.course" method="post" id="searchStudentsByCourseName" role="form" >
-            <h3>Student Performance</h3>
-            <br/>
-            <select name="courseId">
-                <c:forEach var="course" items="${courses}">
-                    <option name="${course.courseId}" value="${course.courseId}">${course.name} </option>
-                </c:forEach>
-            </select>                               
-            <input type="submit" value="FILTER" class="btn-primary">
-        </form>
-
-
-        <table>
-            <tr>
-                <td>
-
-                    <c:choose>
-                        <c:when test="${empty enroledcourses }">
-                            Table is empty.
+            <form action="${pageContext.request.contextPath}/lecturer/filter.course" method="post" id="searchStudentsByCourseName" role="form" >
+                <h3>Student Performance</h3>
+                <br/>
+               
+                
+                <select name="courseId" >
+                    <c:forEach var="course" items="${courses}">
+                        <c:choose>
+                        <c:when test="${course.courseId == currentCourse}">
+                            <option name="${course.courseId}" value="${course.courseId}" selected="selected">${course.name} </option>
                         </c:when>
                         <c:otherwise>
-                             <form action ="${pageContext.request.contextPath}/lecturer/gradecourse?pageId=0">    
+                            <option name="${course.courseId}" value="${course.courseId}">${course.name} </option>
+                        </c:otherwise>
+                        </c:choose>
+                    </c:forEach>
+                </select>                               
+                <input type="submit" value="FILTER" class="btn-primary">
+            </form>
 
-                                <table class="table">
-                                    <thead>
-                                        <tr>
-                                          
-                                            <th>STUDENT NAME</th>
-                                            <th> COURSE NAME </th>
-                                            <th> GRADE POINT </th>                          
-                                        </tr>
-                                    </thead>
-                                    <tbody>
-                                        <c:forEach var="enroled" items="${enroledcourses}"><%--need to get arraylist from controller for students and status from enroled course table--%>
+
+            <table>
+                <tr>
+                    <td>
+
+                        <c:choose>
+                            <c:when test="${empty enroledcourses }">
+                                Table is empty.
+                            </c:when>
+                            <c:otherwise>
+                                <form action ="${pageContext.request.contextPath}/lecturer/gradecourse?pageId=0">    
+
+                                    <table class="table">
+                                        <thead>
                                             <tr>
-                                                
-                                                 <td> <c:out value="${enroled.student.user.firstName} ${enroled.student.user.lastName}"/> </td> 
+
+                                                <th>STUDENT NAME</th>
+                                                <th> COURSE NAME </th>
+                                                <th> GRADE POINT </th> 
+                                                <th></th>
+                                            </tr>
+                                        </thead>
+                                        <tbody>
+                                            <c:forEach var="enroled" items="${enroledcourses}"><%--need to get arraylist from controller for students and status from enroled course table--%>
+                                                <tr>
+
+                                            <form action ="${pageContext.request.contextPath}/lecturer/gradecourse.submit">
+                                                <td> <c:out value="${enroled.student.user.firstName} ${enroled.student.user.lastName}"/> </td> 
                                                 <td> <c:out value="${enroled.course.name}"/> </td>
-                                                <td><input name="${enroled.student.studentId}" id="${enroled.student.studentId}" type="text" value="${enroled.gradePoint}"/></td>       
+                                                <td><input name="grade" id="grade" type="text" value="${enroled.gradePoint}"/></td>       
+                                                <input type="hidden" name="courseId" id="courseId" value="${enroled.course.courseId}"/>
+                                                <input type="hidden" name="studentId" id="studentId" value="${enroled.student.studentId}"/>
+                                                <td>  <input type="submit"/></td>
+
+                                            </form>
                                             </tr>
                                         </c:forEach>   
-                                    </tbody>
-                                </table>
-                                <c:choose>
-                                    <c:when test ="${not empty countEnrol}">
-                                        <div>
-                                            <ul class="pagination">
-                                                <c:forEach var="i" begin="0" end="${countEnrol-1}" >
-                                                    <c:choose>
-                                                        <c:when test="${pageId == i}">
-                                                            <li class="active">  <a  href="${pageContext.request.contextPath}/lecturer/gradecourse?pageId=${i}">${i+1}</a></li>
-                                                            </c:when>
-                                                            <c:otherwise>
-                                                            <li>  <a class="" href="${pageContext.request.contextPath}/lecturer/gradecourse?pageId=${i}">${i+1}</a> </li>
-                                                            </c:otherwise>
-                                                        </c:choose>
-                                                    </c:forEach>
-                                            </ul>
-                                        </div>
-                                    </c:when>
-                                    <c:otherwise>
-                                              
-                                            
+                                        </tbody>
+                                    </table>
+                                    <c:choose>
+                                        <c:when test ="${not empty countEnrol}">
+                                            <div>
+                                                <ul class="pagination">
+                                                    <c:forEach var="i" begin="0" end="${countEnrol-1}" >
+                                                        <c:choose>
+                                                            <c:when test="${pageId == i}">
+                                                                <li class="active">  <a  href="${pageContext.request.contextPath}/lecturer/gradecourse?pageId=${i}">${i+1}</a></li>
+                                                                </c:when>
+                                                                <c:otherwise>
+                                                                <li>  <a class="" href="${pageContext.request.contextPath}/lecturer/gradecourse?pageId=${i}">${i+1}</a> </li>
+                                                                </c:otherwise>
+                                                            </c:choose>
+                                                        </c:forEach>
+                                                </ul>
+                                            </div>
+                                        </c:when>
+                                        <c:otherwise>
+
+
                                             <button type="submit" class="btn btn-primary  btn-md">Back</button> 
                                         </form>
                                     </c:otherwise>
                                 </c:choose>
-                                
-                                        <form method="post" action="${pageContext.request.contextPath}/lecturer/submit.grade">
-                                <input type="submit" value="submit" class="btn btn-primary">
-                            </form>
-                              
-                        </c:otherwise>
-                    </c:choose>
-                </td>
-            </tr>    
-        </table>            
-            
-                    
-</div>
+
+                                <form method="post" action="${pageContext.request.contextPath}/lecturer/submit.grade">
+                                    <input type="submit" value="submit" class="btn btn-primary">
+                                </form>
+
+                            </c:otherwise>
+                        </c:choose>
+                    </td>
+                </tr>    
+            </table>            
+
+
+        </div>
     </body>
 </html>
